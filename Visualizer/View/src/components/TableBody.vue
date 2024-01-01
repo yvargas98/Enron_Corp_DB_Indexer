3c77101e19ee0fd507c4994542064095952f1ec5
Yasmin Vargas <yvargas.vargasgodoy@gmail.com>
2023-12-31 22:40:59 -0600

    Added pagination

--- Visualizer/View/src/components/TableBody.vue
+++ Visualizer/View/src/components/TableBody.vue
@@ -17,19 +17,24 @@ export default {
     },
     created() {
         this.emails = this.data
-        console.log("this.data", this.data)
     },
     updated() {
         this.emails = this.data
     },
     methods: {
-        formatContent(content) {
-            const queryPosition = content.toLowerCase().indexOf(this.value.toLowerCase());
-            const paragraphs = content.split('\n\n');
-            const formattedContent = paragraphs.map(paragraph => `${paragraph}`).join('');
-            const shortContent = (queryPosition > 40 ? "..." : "") + formattedContent.slice(queryPosition < 40 ? 0 : queryPosition - 40, queryPosition + 85) + "..."
-            const highlightedContent = shortContent.replace(this.value, `<span style="font-weight: bold;">${this.value}</span>`);
-            return highlightedContent
+        highlightTextField(text) {
+            if(text.length > 200) {
+                const queryPosition = text.toLowerCase().indexOf(this.value.toLowerCase());
+                const paragraphs = text.split('\n\n');
+                const formattedContent = paragraphs.map(paragraph => `${paragraph}`).join('');
+                const shortContent = (queryPosition > 40 ? "..." : "") + formattedContent.slice(queryPosition < 40 ? 0 : queryPosition - 40, queryPosition + 85) + "..."
+                const highlightedContent = shortContent.replace(this.value, `<span style="font-weight: bold;" class="bg-blue-200">${this.value}</span>`);
+                return highlightedContent
+            }
+            else {
+                const highlightedContent = text.replace(this.value, `<span style="font-weight: bold;" class="bg-blue-200">${this.value}</span>`);
+                return highlightedContent
+            }
         }
     }
 }
@@ -37,14 +42,21 @@ export default {
 
 <template>
     <tbody>
+        <tr v-if="emails.length === 0">
+            <td class="px-4 py-4 text-sm" colspan="4">
+                <div class="text-gray-700 dark:text-gray-500 text-center">
+                    No result found.
+                </div>
+            </td>
+        </tr>
         <tr v-for="(email, i) in emails" :key="i">
-            <td class="px-4 py-4 text-sm text-gray-700 dark:text-gray-500" v-html="formatContent(email.from)">
+            <td class="px-4 py-4 text-sm text-gray-700 dark:text-gray-500 w-64 max-h-40 overflow-y-auto" v-html="highlightTextField(email.from)">
             </td>
-            <td class="px-4 py-4 text-sm text-gray-700 dark:text-gray-500" v-html="formatContent(email.to)">
+            <td class="px-4 py-4 text-sm text-gray-700 dark:text-gray-500 w-64 max-h-40 overflow-y-auto" v-html="highlightTextField(email.to)">
             </td>
-            <td class="px-4 py-4 text-sm text-gray-700 dark:text-gray-500" v-html="formatContent(email.subject)">
+            <td class="px-4 py-4 text-sm text-gray-700 dark:text-gray-500 w-64 max-h-40 overflow-y-auto" v-html="highlightTextField(email.subject)">
             </td>
-            <td class="px-4 py-4 text-sm text-gray-700 dark:text-gray-500" v-html="formatContent(email.content)">
+            <td class="px-4 py-4 text-sm text-gray-700 dark:text-gray-500 w-64 max-h-40 overflow-y-auto" v-html="highlightTextField(email.content)">
             </td>
         </tr>
     </tbody>
--- Visualizer/View/src/views/MainView.vue
+++ Visualizer/View/src/views/MainView.vue
@@ -4,6 +4,7 @@ import Input from "../components/Input.vue";
 import Button from "../components/Button.vue";
 import TableHeaders from "../components/TableHeaders.vue";
 import TableBody from "../components/TableBody.vue";
+import Pagination from "../components/Pagination.vue";
 
 import { SearchServices } from "../services/service.js"
 
@@ -13,7 +14,8 @@ export default {
         Input,
         Button,
         TableHeaders,
-        TableBody
+        TableBody,
+        Pagination
     },
     created() {
         this.searchService = new SearchServices()
@@ -26,6 +28,7 @@ export default {
             value: "",
             offset: 1,
             limit: 10,
+            totalResults: 0,
             emails: []
         }
     },
@@ -37,22 +40,32 @@ export default {
                 from: this.offset,
                 size: this.limit
             }
-            const content = await this.searchService.searchContents(request)
-            const formatEmails = content.map((item)=> ({
-                content: item.content,
-                date: item.data,
-                from: item.from,
-                subject: item.subject,
-                to: item.to,
-            }))
-            this.setEmails(formatEmails)
+            if (this.value!= "") {
+                const content = await this.searchService.searchContents(request)
+                const formatEmails = content.map((item)=> ({
+                    content: item.content,
+                    date: item.data,
+                    from: item.from,
+                    subject: item.subject,
+                    to: item.to,
+                }))
+                this.setEmails(formatEmails)
+                this.totalResults = formatEmails.length;
+            }
+            else {
+                this.setEmails([]);
+                this.totalResults = 0;
+            }
         },
         setEmails(newEmails) {
             this.emails = [...newEmails]
-            console.log("this.emails 2", this.emails)
         },
         created() {
             this.emails = [""]
+        },
+        async updateOffset(newOffset) {
+            this.offset = newOffset;
+            await this.searchContents();
         },
     }
 }
@@ -62,23 +75,6 @@ export default {
     <MamuroHeader/>
     <section class="container px-4 mx-auto">
         <div class="mt-6 md:flex md:items-center md:justify-between">
-            <!-- <div class="inline-flex overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700">
-                <button class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 bg-gray-100 sm:text-sm dark:bg-gray-800 dark:text-gray-300">
-                    Match all
-                </button>
-                <button class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-                    Search by From
-                </button>
-                <button class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-                    Search by To
-                </button>
-                <button class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-                    Search by Subject
-                </button>
-                <button class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 sm:text-sm dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">
-                    Search in Content
-                </button>
-            </div>  -->
             <div class="relative flex items-center mt-4 md:mt-0">
                 <span class="absolute">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600">
@@ -100,6 +96,7 @@ export default {
                     </div>
                 </div>
             </div>
+            <Pagination :offset="offset" :limit="limit" :totalResults="totalResults" @update:offset="updateOffset"></Pagination>
         </div>
     </section>
     
